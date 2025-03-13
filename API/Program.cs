23400da329b1529ad7375baaa06185d83afbdfc8
Mohammed Tariq Al-Ghamdi <moohammedtg208@outlook.com>
2025-03-13 21:22:23 +0300

    end of section 2

--- API/Program.cs
+++ API/Program.cs
@@ -5,12 +5,21 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
+builder.Services.AddCors();
 
 builder.Services.AddDbContext<DataContext>(opt=>{
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")); 
 });
 
 var app = builder.Build();
+
+// Add CORS middleware (order is important - put this before UseAuthorization)
+app.UseCors(
+    policy=>policy
+    .AllowAnyHeader()
+    .AllowAnyMethod()
+    .WithOrigins("http://localhost:4200","https://localhost:4200")
+);
 
 app.MapControllers();
 
